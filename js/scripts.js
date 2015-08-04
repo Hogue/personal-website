1ded920fa9fb703011eeb2154a321f8cd043e001
Andrew Hogue <Hogue@Andrews-MacBook-Pro.local>
2015-08-03 23:17:52 -0400

    first commitportfolio website

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+  $('.div-hidden').fadeIn(3000).removeClass('hidden');
+
+  // vpw = $(window).width();
+  // vph = $(window).height();
+
+  // $('.full-page').height(vph);
+
+});
